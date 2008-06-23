53d9bf5067c31d1fe22444f6ef121b83758ec63c
arunchaganty <arunchaganty@bc68ed02-4b4e-0410-980a-65d8c96c09fd>
2008-06-23 23:44:25 +0000

    * Added VimWidget files * Removed unnecessary files * Added self to author :-P *
    git-svn-id: https://anjuta-gvim.googlecode.com/svn/trunk@16 bc68ed02-4b4e-0410-980a-65d8c96c09fd

---
+++ src/vim-widget.c
@@ -0,0 +1,161 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * anjuta-gvim
+ * Copyright (C) Arun Tejasvi Chaganty 2008 <arunchaganty@gmail.com>
+ * 
+ * anjuta-gvim is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * anjuta-gvim is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This actually takes care of all transactions with Vim.
+ */
+
+#include <libanjuta/anjuta-debug.h>
+#include <gtk/gtk.h>
+#include "vim-widget.h"
+#include "vim-dbus.h"
+
+#define VIM_WIDGET_PRIVATE(o)  (G_TYPE_INSTANCE_GET_PRIVATE ((o), VIM_TYPE_WIDGET, VimWidgetPrivate))
+#define VIM_DBUS_FILE "/home/teju/Projects/anjuta-gvim/src/vim-dbus.py"
+#define GVIMRC_FILE "/home/teju/Projects/anjuta-gvim/misc/anjuta.gvimrc"
+
+/* 
+ * Once the window is parented, call this function to embed vim
+ */
+
+static GObjectClass* parent_class;
+
+static void 
+vim_widget_connect_plug (VimWidget *widget, GParamSpec *param) 
+{
+	gchar *cmd;
+	GError *err = NULL;
+	GtkNotebook *parent;
+
+	g_object_get (widget,
+			"parent", &parent,
+			NULL);
+
+
+	if (parent != NULL)
+	{
+		widget->priv->socket_id = gtk_socket_get_id ((GtkSocket *) widget->priv->socket);
+		g_assert (widget->priv->socket_id != 0);
+		
+		cmd = g_strdup_printf ("gvim -U %s --socketid %d \n",
+				GVIMRC_FILE, widget->priv->socket_id);
+		g_message ("Executing %s\n", cmd);
+		/* Run vim */
+		g_spawn_command_line_async (cmd, &err);
+		g_free (cmd);
+
+		if (err)
+		{
+			DEBUG_PRINT ("VimPlugin: Error: %s", err);
+			g_object_unref (err);
+			err = NULL;
+		}
+
+		/* Connect callbacks */
+	}
+
+}
+
+static GObject*
+vim_widget_constructor ( GType   type,
+						guint   n_construct_properties,
+						GObjectConstructParam *construct_properties)
+{
+    GObject* obj;
+	GError *err = NULL;
+    VimWidgetClass* klass;
+	VimWidget* widget;
+	VimWidgetPrivate *priv;
+	
+    klass = VIM_WIDGET_CLASS (g_type_class_peek (VIM_TYPE_WIDGET));
+
+	if (klass->widget)
+		return g_object_ref (klass->widget);
+
+	widget = VIM_WIDGET (parent_class->constructor (type,
+						   n_construct_properties,
+						   construct_properties));
+	klass->widget = widget;
+	priv = widget->priv;
+
+	/* Create a socket */
+	priv->socket = (GtkSocket*) g_object_new (GTK_TYPE_SOCKET,
+			"visible", TRUE,
+			NULL);
+	g_assert (widget != NULL && priv->socket != NULL);
+	gtk_container_add (GTK_CONTAINER(widget), GTK_WIDGET(priv->socket));
+	g_signal_connect_after (widget,
+						"notify::parent",
+						G_CALLBACK(vim_widget_connect_plug),
+						NULL);
+	
+	// Now hook in DBus
+	vim_dbus_init(widget, &err);
+	if (err) 
+	{
+		DEBUG_PRINT ("VimPlugin: Error: %s", err);
+		g_object_unref (err);
+		err = NULL;
+	}
+
+	return G_OBJECT(widget);
+}
+
+static void
+vim_widget_init (VimWidget *widget)
+{
+	widget->priv = g_new0 (VimWidgetPrivate, 1);
+	widget->priv->documents = NULL; /* NULL is the empty GList */
+}
+
+static void
+vim_widget_finalize (GObject *object)
+{
+	/* TODO: Add deinitalization code here */
+
+	VimWidget* widget = VIM_WIDGET (object);
+    VimWidgetClass* klass = VIM_WIDGET_CLASS (g_type_class_peek (VIM_TYPE_WIDGET));
+	VimWidgetPrivate *priv = VIM_WIDGET_PRIVATE(widget);
+	
+	g_list_free (priv->documents);
+
+	g_free (priv->conn);
+	g_free (priv->dbus_proxy);
+	g_free (priv->proxy);
+
+	klass->widget = NULL;
+	parent_class->finalize (object);
+}
+
+static void
+vim_widget_class_init (VimWidgetClass *klass)
+{
+    parent_class = G_OBJECT_CLASS (g_type_class_peek_parent (klass));
+	GObjectClass* object_class = G_OBJECT_CLASS (klass);
+
+	g_type_class_add_private (klass, sizeof (VimWidgetPrivate));
+	object_class->constructor = vim_widget_constructor;
+	object_class->finalize = vim_widget_finalize;
+
+	klass->widget = NULL; /* Singleton hasn't been initialized yet */
+
+}
+
+G_DEFINE_TYPE (VimWidget, vim_widget, GTK_TYPE_FRAME);
+
---
+++ src/vim-widget.h
@@ -0,0 +1,70 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * anjuta-gvim
+ * Copyright (C) Arun Tejasvi Chaganty 2008 <arunchaganty@gmail.com>
+ * 
+ * anjuta-gvim is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * anjuta-gvim is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _VIM_WIDGET_H_
+#define _VIM_WIDGET_H_
+
+#include <glib-object.h>
+#include <gtk/gtk.h>
+#include <dbus/dbus-glib.h>
+#include <libanjuta/anjuta-plugin.h>
+
+G_BEGIN_DECLS
+
+#define VIM_TYPE_WIDGET             (vim_widget_get_type ())
+#define VIM_WIDGET(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), VIM_TYPE_WIDGET, VimWidget))
+#define VIM_WIDGET_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), VIM_TYPE_WIDGET, VimWidgetClass))
+#define VIM_IS_WIDGET(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), VIM_TYPE_WIDGET))
+#define VIM_IS_WIDGET_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), VIM_TYPE_WIDGET))
+#define VIM_WIDGET_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), VIM_TYPE_WIDGET, VimWidgetClass))
+#define VIM_PLUGIN_IS_READY(widget)	((widget->priv->socket_id != 0) && (widget->priv->proxy != NULL))
+
+typedef struct _VimWidgetClass VimWidgetClass;
+typedef struct _VimWidget VimWidget;
+typedef struct _VimWidgetPrivate VimWidgetPrivate;
+
+struct _VimWidgetClass
+{
+	GtkFrameClass parent_class;
+	VimWidget *widget; // Singleton
+};
+
+struct _VimWidget
+{
+	GtkFrame parent_instance;
+	VimWidgetPrivate* priv;
+
+};
+
+struct _VimWidgetPrivate
+{
+	gint socket_id;
+	GtkSocket* socket;
+	DBusGConnection* conn;
+	DBusGProxy* dbus_proxy;
+	DBusGProxy* proxy;
+
+	GList* documents; /* Open documents */
+};
+
+GType vim_widget_get_type (void) G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif /* _VIM_WIDGET_H_ */
