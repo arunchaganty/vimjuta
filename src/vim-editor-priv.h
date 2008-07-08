30dde881e395911b86ecfee11b0e8c043f3950dc
arunchaganty <arunchaganty@bc68ed02-4b4e-0410-980a-65d8c96c09fd>
2008-07-08 20:58:14 +0000

    Added missing headers. Thanks to yann.leydier for mentioning this.
    git-svn-id: https://anjuta-gvim.googlecode.com/svn/trunk@32 bc68ed02-4b4e-0410-980a-65d8c96c09fd

---
+++ src/vim-editor-priv.h
@@ -0,0 +1,38 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * anjuta-gvim
+ * Copyright (C) Arun Tejasvi Chaganty 2008 <arunchaganty@gmail.com>
+ * 
+ * anjuta-gvim is free software.
+ * 
+ * You may redistribute it and/or modify it under the terms of the
+ * GNU General Public License, as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * anjuta-gvim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with anjuta-gvim.  If not, write to:
+ * 	The Free Software Foundation, Inc.,
+ * 	51 Franklin Street, Fifth Floor
+ * 	Boston, MA  02110-1301, USA.
+ */
+
+#ifndef _VIM_EDITOR_PRIV_H_
+#define _VIM_EDITOR_PRIV_H_
+
+#include <glib-object.h>
+#include "vim-widget.h"
+
+struct _VimEditorPrivate
+{
+	GFile *file;
+	gint bufno;
+	VimWidget *widget; /* Note, this is a singleton */
+};
+
+#endif /* _VIM_EDITOR_PRIV_H_ */
